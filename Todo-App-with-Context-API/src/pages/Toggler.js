9f69390cd720b33d0c09e7d8cbd5a2b495540975
Manoj Kumar <manojmani517@gmail.com>
2020-02-20 21:33:16 +0530

    Consumed multiple contexts in 'Functional component' with 'useContext()' in 'Togglers.js' file --> Todo-App-with-Context-API

--- Todo-App-with-Context-API/src/pages/Toggler.js
+++ Todo-App-with-Context-API/src/pages/Toggler.js
@@ -1,30 +1,19 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "./../contexts/ThemeContext";
 import { AuthContext } from "./../contexts/AuthContext";
 
-class ThemeToggle extends Component {
-  static contextType = ThemeContext;
-  render() {
-    let { toggleTheme } = this.context;
-    return (
-      <AuthContext.Consumer>
-        {authContext => {
-          const { isAuthenticated, toggleAuth } = authContext;
-          let checkAuth = isAuthenticated
-            ? "Authenticated"
-            : "Un-Authenticated";
-          return (
-            <div style={{ margin: "20px" }}>
-              <button onClick={toggleTheme} style={{ marginRight: "15px" }}>
-                Toggle Theme
-              </button>
-              <button onClick={toggleAuth}>{checkAuth}</button>
-            </div>
-          );
-        }}
-      </AuthContext.Consumer>
-    );
-  }
-}
+const ThemeToggle = props => {
+  let { toggleTheme } = useContext(ThemeContext);
+  let { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  let checkAuth = isAuthenticated ? "Authenticated" : "Un-Authenticated";
+  return (
+    <div style={{ margin: "20px" }}>
+      <button onClick={toggleTheme} style={{ marginRight: "15px" }}>
+        Toggle Theme
+      </button>
+      <button onClick={toggleAuth}>{checkAuth}</button>
+    </div>
+  );
+};
 
 export default ThemeToggle;
