01471c8197c0703740943feac457f46bed18af12
Manoj Kumar <manojmani517@gmail.com>
2020-02-26 00:08:42 +0530

    Implemeted functionality of adding new contact and running Backend & Client app with one command --> Contact-Keeper

--- Contact-Keeper/Backend/routes/contacts.js
+++ Contact-Keeper/Backend/routes/contacts.js
@@ -24,9 +24,40 @@ router.get("/", auth, async (req, res) => {
 // @route       POST api/contacts
 // @desc        Add new contact
 // @access      Private
-router.post("/", (req, res) => {
-  res.send(`Add new contact`);
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("name", "Name is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Server Error - save contact");
+    }
+  }
+);
 
 // @route       PUT api/contacts/:id
 // @desc        Update contact
