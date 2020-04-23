391394339788a44f077bf5cacab824fb355c8c1a
Manoj Kumar <manojmani517@gmail.com>
2020-04-23 23:24:00 +0530

    Added Models & Middlewares folders --> My-Pro

---
+++ My-Pro/Backend/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = (module.exports = mongoose.model("user", UserSchema));
+
+module.exports.getUserById = function (id, callback) {
+  User.findById(id, callback);
+};
+
+module.exports.getUserByUsername = function (username, callback) {
+  const query = { username: username };
+  User.findOne(query, callback);
+};
+
+module.exports.addUser = function (newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+};
+
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if (err) throw err;
+    callback(null, isMatch);
+  });
+};
--- My-Pro/Backend/routes/users.js
+++ My-Pro/Backend/routes/users.js
@@ -1,8 +1,92 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const auth = require("../middlewares/auth");
 
-router.get("/", (req, res) => {
+const User = require("../models/User");
+
+// sample test route
+router.get("/test/", (req, res) => {
   res.status(200).json({ msg: "This is Users home route" });
+});
+
+// @route       POST api/users
+// @desc        Register a user
+// @access      Public
+router.post(
+  "/",
+  [
+    check("name", "Please add name").not().isEmpty(),
+    check("email", "Please include valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more charactors"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      console.log("User object is below");
+      console.log(user);
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route     GET api/users
+// @desc      Get all users
+// @access    Private
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().sort({
+      date: -1,
+    });
+    res.json(users);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
