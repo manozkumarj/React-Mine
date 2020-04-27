01b404d5fa65116b359956f324111189a8848eef
Manoj Kumar <manojmani517@gmail.com>
2020-04-27 15:49:29 +0530

    Implemented single picture post thrice with 3 diff pics --> My-Pro

--- My-Pro/src/components/layouts/middleSection/MiddleSection.jsx
+++ My-Pro/src/components/layouts/middleSection/MiddleSection.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./middleSection.css";
-import zuck from "../../../images/zuck.jpg";
-import mark from "../../../images/mark.jpg";
+// import zuck from "../../../images/zuck.jpg";
+// import mark from "../../../images/mark.jpg";
+
+import kohli from "../../../images/kohli.jpg";
+import bikee from "../../../images/bikee.jpg";
+import wow1 from "../../../images/wow_1.jpg";
+import wow2 from "../../../images/wow_2.jpg";
 
 const MiddleSection = () => {
   return (
@@ -39,89 +44,83 @@ const MiddleSection = () => {
 
       {/* *******************  All posts section ******************** */}
       <div className="all-posts-container">
-        <p style={{ margin: "20px 0px" }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        {/* *******************  Single picture posts section - starts ******************** */}
+        <div className="item">
+          <div className="post-n-user-details-container">
+            <div className="post-dp-div">
+              <img className="post-user-dp" src={kohli} alt="Post user DP" />
+            </div>
+            <div className="postInfo-n-user-details-div">
+              <div className="post-details-div">Manoj Kumar</div>
+              <div className="post-timestamp-div">
+                5th Jan 2017 - 08:51:25 AM
+              </div>
+            </div>
+          </div>
+          <div className="post-description-div">
+            <span className="post-description">
+              This is my first test for updates div. Just to check whether it's
+              working or not.This is my first test for updates div. Just to
+              check whether it's working or not.
+            </span>
+          </div>
+          <div className="post-picture-div">
+            <img src={wow1} alt="Post picture" />
+          </div>
+        </div>
+        {/* *******************  Single picture posts section - ends ******************** */}
 
-        <p style={{ margin: "20px 0px" }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        {/* *******************  Single picture posts section - starts ******************** */}
+        <div className="item">
+          <div className="post-n-user-details-container">
+            <div className="post-dp-div">
+              <img className="post-user-dp" src={kohli} alt="Post user DP" />
+            </div>
+            <div className="postInfo-n-user-details-div">
+              <div className="post-details-div">Manoj Kumar</div>
+              <div className="post-timestamp-div">
+                5th Jan 2017 - 08:51:25 AM
+              </div>
+            </div>
+          </div>
+          <div className="post-description-div">
+            <span className="post-description">
+              This is my first test for updates div. Just to check whether it's
+              working or not.This is my first test for updates div. Just to
+              check whether it's working or not.
+            </span>
+          </div>
+          <div className="post-picture-div">
+            <img src={wow2} alt="Post picture" />
+          </div>
+        </div>
+        {/* *******************  Single picture posts section - ends ******************** */}
 
-        <p style={{ margin: "20px 0px" }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-
-        <p style={{ margin: "20px 0px" }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-
-        <p style={{ margin: "20px 0px" }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-
-        <p style={{ margin: "20px 0px" }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
-
-        <p style={{ margin: "20px 0px" }}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </p>
+        {/* *******************  Single picture posts section - starts ******************** */}
+        <div className="item">
+          <div className="post-n-user-details-container">
+            <div className="post-dp-div">
+              <img className="post-user-dp" src={kohli} alt="Post user DP" />
+            </div>
+            <div className="postInfo-n-user-details-div">
+              <div className="post-details-div">Manoj Kumar</div>
+              <div className="post-timestamp-div">
+                5th Jan 2017 - 08:51:25 AM
+              </div>
+            </div>
+          </div>
+          <div className="post-description-div">
+            <span className="post-description">
+              This is my first test for updates div. Just to check whether it's
+              working or not.This is my first test for updates div. Just to
+              check whether it's working or not.
+            </span>
+          </div>
+          <div className="post-picture-div">
+            <img src={bikee} alt="Post picture" />
+          </div>
+        </div>
+        {/* *******************  Single picture posts section - ends ******************** */}
       </div>
     </div>
   );
