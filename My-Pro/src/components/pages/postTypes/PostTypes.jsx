19473b850638d0df8e98c3f0de6698cea991540c
Manoj Kumar <manojmani517@gmail.com>
2020-05-01 08:34:00 +0530

    Showing only alert onclicking individual post type. About to work on showing post type modal --> My-Pro

--- My-Pro/src/components/pages/postTypes/PostTypes.jsx
+++ My-Pro/src/components/pages/postTypes/PostTypes.jsx
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import "./postTypes.css";
 import RightSideSection from "./../../layouts/rightSideSection/RightSideSection";
 
+import overlayClose from "../../../images/overlay-close.png";
+import fancyClose from "../../../images/fancy-close.png";
+
 export default function PostTypes() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -18,13 +21,19 @@ export default function PostTypes() {
 
         {/* Background & Text color type */}
         <div className="types-container">
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-type"
+          >
             <div className="individual-type-inner type-bg-color">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
             </div>
           </div>
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-type"
+          >
             <div className="individual-type-inner type-text-color">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
@@ -40,7 +49,11 @@ export default function PostTypes() {
 
         {/* Background, Text & border color type */}
         <div className="types-container">
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="solid-all"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-solid-border">
                 Lorem Ipsum is simply dummy text of the printing.
@@ -48,7 +61,11 @@ export default function PostTypes() {
             </div>
           </div>
 
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="dashed-all"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-dashed-border">
                 Lorem Ipsum is simply dummy text of the printing.
@@ -56,7 +73,11 @@ export default function PostTypes() {
             </div>
           </div>
 
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="dotted-all"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-dotted-border">
                 Lorem Ipsum is simply dummy text of the printing.
@@ -64,7 +85,11 @@ export default function PostTypes() {
             </div>
           </div>
 
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="double-all"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-double-border">
                 Lorem Ipsum is simply dummy text of printing.
@@ -74,28 +99,44 @@ export default function PostTypes() {
         </div>
 
         <div className="types-container">
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="solid-lNr"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-solid-border border-lNr">
                 Lorem Ipsum is simply dummy text of the printing.
               </div>
             </div>
           </div>
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="solid-tNb"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-solid-border border-tNb">
                 Lorem Ipsum is simply dummy text of the printing.
               </div>
             </div>
           </div>
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="dashed-lNr"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-dashed-border border-lNr">
                 Lorem Ipsum is simply dummy text of the printing.
               </div>
             </div>
           </div>
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="dashed-tNb"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-dashed-border border-tNb">
                 Lorem Ipsum is simply dummy text of the printing.
@@ -105,28 +146,44 @@ export default function PostTypes() {
         </div>
 
         <div className="types-container">
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="dotted-lNr"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-dotted-border border-lNr">
                 Lorem Ipsum is simply dummy text of the printing.
               </div>
             </div>
           </div>
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="dotted-tNb"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-dotted-border border-tNb">
                 Lorem Ipsum is simply dummy text of the printing.
               </div>
             </div>
           </div>
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="double-lNr"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-double-border border-lNr">
                 Lorem Ipsum is simply dummy text of printing.
               </div>
             </div>
           </div>
-          <div className="individual-type">
+          <div
+            className="individual-type open-post-type-modal"
+            id="bg-N-text-N-border-type"
+            data-post-type="double-tNb"
+          >
             <div className="individual-type-inner type-bg-color-1">
               <div className="inner-border inner-double-border border-tNb">
                 Lorem Ipsum is simply dummy text of printing.
@@ -138,6 +195,46 @@ export default function PostTypes() {
 
       <div className="right-section">
         <RightSideSection />
+      </div>
+
+      {/* Post type modals below - Layerone - starts */}
+
+      <div
+        id="layerOneModalContainer"
+        tabIndex="1"
+        className="layerOneModalContainer"
+      >
+        <span className="overlay_close layerOneCloser" title="close">
+          <img height="20" src={overlayClose} alt="closer" />
+        </span>
+
+        <div className="layerOneModalInner">
+          <div className="sample-LayerOne">
+            <span className="fancyCloseIcon layerOneCloser" title="close">
+              <img height="30" src={fancyClose} alt="closer" />
+            </span>
+
+            <button className="layerOneCloser" type="button">
+              close Modal
+            </button>
+            <br />
+            <button className="openLayerTwoModal" type="button">
+              Open 2nd Modal
+            </button>
+            <br />
+            <br />
+
+            <p>
+              Manoj Kumar - Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Mauris ac ornare metus. Duis luctus consequat consectetur.
+              Integer eleifend metus et condimentum rutrum. Nam vel laoreet
+              diam, in gravida metus. Etiam sagittis diam nec lacus sodales, id
+              hendrerit quam convallis. Proin finibus pretium commodo. Nam et
+              luctus metus. Nulla vitae augue est. Donec vitae tristique urna,
+              id cursus nunc.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
