987e9df646fca4695915c03fc0b834b01a9f5dff
Manoj Kumar <manojmani517@gmail.com>
2020-05-03 19:32:31 +0530

    Basic look for right side div for Friend suggessions is done. About to work on site footer div --> My-Pro

--- My-Pro/src/components/layouts/rightSideSection/RightSideSection.jsx
+++ My-Pro/src/components/layouts/rightSideSection/RightSideSection.jsx
@@ -1,21 +1,149 @@
 import React from "react";
 import "./rightSideSection.css";
+import { Link } from "react-router-dom";
+import zuck from "../../../images/zuck.jpg";
+import mark from "../../../images/mark.jpg";
+import kohli from "../../../images/kohli.jpg";
 
 const RightSideSection = () => {
   return (
     <div className="fixed-div" id="right-fixed-div">
-      <p style={{ margin: "20px 10px" }}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <div className="friend-suggessions-container">
+        <div className="friend-suggessions-title">Friend Suggessions</div>
 
+        {/* List of friend suggessions - starts */}
+        <div className="friend-suggession">
+          <div className="friend-suggession-dps-container">
+            <img
+              src={zuck}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-primary-dp"
+            />
+            <img
+              src={mark}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-secondary-dp"
+            />
+          </div>
+          <div className="friend-suggession-user-details-div">
+            <div className="friend-suggession-user-name">
+              <Link to="/" className="global-aTag-style">
+                Manoj Kumar
+              </Link>
+            </div>
+            <div className="friend-suggession-button">
+              <button className="suggession-request-friendshp-btn">
+                Request Friendship
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="friend-suggession">
+          <div className="friend-suggession-dps-container">
+            <img
+              src={kohli}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-primary-dp"
+            />
+            <img
+              src={mark}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-secondary-dp"
+            />
+          </div>
+          <div className="friend-suggession-user-details-div">
+            <div className="friend-suggession-user-name">
+              <Link to="/" className="global-aTag-style">
+                Manoj Kumar
+              </Link>
+            </div>
+            <div className="friend-suggession-button">
+              <button className="suggession-request-friendshp-btn">
+                Request Friendship
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="friend-suggession">
+          <div className="friend-suggession-dps-container">
+            <img
+              src={zuck}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-primary-dp"
+            />
+            <img
+              src={kohli}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-secondary-dp"
+            />
+          </div>
+          <div className="friend-suggession-user-details-div">
+            <div className="friend-suggession-user-name">
+              <Link to="/" className="global-aTag-style">
+                Manoj Kumar
+              </Link>
+            </div>
+            <div className="friend-suggession-button">
+              <button className="suggession-request-friendshp-btn">
+                Request Friendship
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="friend-suggession">
+          <div className="friend-suggession-dps-container">
+            <img
+              src={mark}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-primary-dp"
+            />
+            <img
+              src={zuck}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-secondary-dp"
+            />
+          </div>
+          <div className="friend-suggession-user-details-div">
+            <div className="friend-suggession-user-name">
+              <Link to="/" className="global-aTag-style">
+                Manoj Kumar
+              </Link>
+            </div>
+            <div className="friend-suggession-button">
+              <button className="suggession-request-friendshp-btn">
+                Request Friendship
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="friend-suggession">
+          <div className="friend-suggession-dps-container">
+            <img
+              src={zuck}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-primary-dp"
+            />
+            <img
+              src={mark}
+              alt="User name"
+              className="friend-suggession-dps friend-suggession-secondary-dp"
+            />
+          </div>
+          <div className="friend-suggession-user-details-div">
+            <div className="friend-suggession-user-name">
+              <Link to="/" className="global-aTag-style">
+                Manoj Kumar
+              </Link>
+            </div>
+            <div className="friend-suggession-button">
+              <button className="suggession-request-friendshp-btn">
+                Request Friendship
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* List of friend suggessions - ends */}
+      </div>
       <button className="openLayerOneModal">Click me</button>
     </div>
   );
