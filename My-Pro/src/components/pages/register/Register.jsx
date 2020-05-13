f1f60161bf4a0e79970f71e0d991c7afd4b6466c
Manoj Kumar <manojmani517@gmail.com>
2020-05-13 16:17:42 +0530

    Basic look of Register page is done. About to work on Login page --> My-Pro

--- My-Pro/src/components/pages/register/Register.jsx
+++ My-Pro/src/components/pages/register/Register.jsx
@@ -1,9 +1,64 @@
 import React, { useEffect } from "react";
+import "./register.css";
+import { Link } from "react-router-dom";
 
-export default function Register() {
+const Register = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  return <div>This is Register page</div>;
-}
+  return (
+    <div className="three-divs-container">
+      <div id="register-main-container">
+        <div className="reg-title">
+          Register an account, It's free and always will be
+        </div>
+
+        <hr className="dividable-hr" />
+
+        <div className="reg-form-holder">
+          <form autoComplete="off">
+            <div className="reg-form-field-holder">
+              <input
+                type="text"
+                name="fullName"
+                className="reg-form-field"
+                placeholder="Enter Fullname"
+              />
+            </div>
+            <div className="reg-form-field-holder">
+              <input
+                type="text"
+                name="email"
+                className="reg-form-field"
+                placeholder="Enter Email"
+              />
+            </div>
+            <div className="reg-form-field-holder">
+              <input
+                type="text"
+                name="password"
+                className="reg-form-field"
+                placeholder="New password"
+              />
+            </div>
+            <div className="reg-form-field-holder">
+              <button type="button" className="reg-form-btn">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="width-75">
+          <hr className="dividable-hr" />
+        </div>
+        <div className="login-container">
+          <Link to="/login" className="global-aTag-style">
+            Log In to Existing Account
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Register;
