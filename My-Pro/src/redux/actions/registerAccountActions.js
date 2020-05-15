87a5530dad77fde9efdc392e3b3bb80c2954e5eb
Manoj Kumar <manojmani517@gmail.com>
2020-05-15 10:00:50 +0530

    Made sample API call using AXIOS to backend in actionCreator --> My-Pro

--- My-Pro/src/redux/actions/registerAccountActions.js
+++ My-Pro/src/redux/actions/registerAccountActions.js
@@ -1,8 +1,23 @@
+import API from "./../../api";
 import { REGISTRATION_SUCCESS } from "./../actionTypes/registerAccountTypes";
+import { REGISTRATION_FAILED } from "./../actionTypes/registerAccountTypes";
 
 export const registerAccount = (accountDetails) => {
-  console.log(accountDetails);
+  // console.log(accountDetails);
+
+  // API.post(`users`, { accountDetails })
+  //   .then((res) => console.log(res))
+  //   .catch((err) => console.log(err));
+
   return (dispatch) => {
-    dispatch({ type: REGISTRATION_SUCCESS, payload: accountDetails });
+    API.get(`users`)
+      .then((res) => {
+        console.log(res.data);
+        dispatch({ type: REGISTRATION_SUCCESS, payload: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: REGISTRATION_FAILED });
+      });
   };
 };
