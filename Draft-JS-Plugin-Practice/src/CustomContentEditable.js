59d9c7cd6565d888bcbd17025c0873308f80671e
Manoj Kumar <manojmani517@gmail.com>
2020-08-05 08:14:48 +0530

    Arrow keys are working as per requirement while showing mentionable users list, and after pressing Enter, user's username is appending to the contenteditable div, about to work on image with username

--- Draft-JS-Plugin-Practice/src/CustomContentEditable.js
+++ Draft-JS-Plugin-Practice/src/CustomContentEditable.js
@@ -6,36 +6,49 @@ import zuck from "./images/zuck.jpg";
 import kohli from "./images/kohli.jpg";
 
 export default function CustomContentEditable() {
+  const [plainText, setPlainText] = useState("");
   const [isItSpace, setIsItSpace] = useState(false);
   const [showMentionsContainer, setShowMentionsContainer] = useState(false);
   const [showTagsContainer, setShowTagsContainer] = useState(false);
   const [showEmojisContainer, setShowEmojisContainer] = useState(false);
+  const [geneateMentionableUser, setGeneateMentionableUser] = useState(null);
 
   const [mentionableUsers, setMentionableUsers] = useState([
     {
       _id: 1,
+      username: "ManozKumarj",
       name: "Manoj Kumar",
       photo: kohli,
     },
     {
       _id: 2,
+      username: "MaheshKumarj",
       name: "Mahesh Kumar",
       photo: zuck,
     },
     {
       _id: 3,
+      username: "Rajj",
       name: "Raj Kumar",
       photo: avatar,
     },
     {
       _id: 4,
+      username: "Malli",
       name: "Mallesh",
       photo: kohli,
     },
     {
       _id: 5,
+      username: "Ravi",
       name: "Ravi Kiran",
       photo: zuck,
+    },
+    {
+      _id: 6,
+      username: "Kranthi",
+      name: "Kranthi Kiran",
+      photo: kohli,
     },
   ]);
 
@@ -43,7 +56,7 @@ export default function CustomContentEditable() {
 
   var mentionsListIndex = 0;
   var mentionsListHighlightItem = 1;
-  var firstList = document.getElementsByClassName(
+  var mentionsUsersList = document.getElementsByClassName(
     "individual-mention-container"
   );
 
@@ -67,7 +80,7 @@ export default function CustomContentEditable() {
       setShowTagsContainer(false);
       setShowEmojisContainer(false);
 
-      firstList[0].classList.add("active");
+      mentionsUsersList[0].classList.add("active");
 
       document.addEventListener("keydown", handleHightlightList);
     }
@@ -97,24 +110,28 @@ export default function CustomContentEditable() {
         e.which
     );
 
-    [].forEach.call(firstList, function (el) {
+    [].forEach.call(mentionsUsersList, function (el) {
       el.classList.remove("active");
     });
 
     if (e.which === 38) {
       if (mentionsListHighlightItem === 1) {
-        mentionsListIndex = firstList.length - 1;
-        mentionsListHighlightItem = firstList.length;
+        mentionsListIndex = mentionsUsersList.length - 1;
+        mentionsListHighlightItem = mentionsUsersList.length;
+        mentionsUsersList[mentionsListIndex].classList.add("active");
+      } else {
+        mentionsUsersList[--mentionsListIndex].classList.add("active");
+        --mentionsListHighlightItem;
       }
-      firstList[--mentionsListIndex].classList.add("active");
-      --mentionsListHighlightItem;
     } else if (e.which === 40) {
-      if (mentionsListHighlightItem === firstList.length) {
+      if (mentionsListHighlightItem === mentionsUsersList.length) {
         mentionsListIndex = 0;
         mentionsListHighlightItem = 1;
+        mentionsUsersList[mentionsListIndex].classList.add("active");
+      } else {
+        mentionsUsersList[++mentionsListIndex].classList.add("active");
+        ++mentionsListHighlightItem;
       }
-      firstList[++mentionsListIndex].classList.add("active");
-      ++mentionsListHighlightItem;
     } else if (e.which === 13) {
       handleIndividualMention();
       setShowMentionsContainer(false);
@@ -130,10 +147,71 @@ export default function CustomContentEditable() {
     console.log("Need to show contentEditable text");
     let wholeContent = contentEditableDiv.textContent.trim();
     console.log(wholeContent);
+    setPlainText(wholeContent);
   };
 
   const handleIndividualMention = () => {
     console.log("handleIndividualMention clicked");
+    console.log(mentionableUsers[mentionsListIndex]);
+
+    let getMentionableUserDetails = mentionableUsers[mentionsListIndex];
+
+    let doGeneateMentionableUser = (
+      <span data-key="1">{getMentionableUserDetails.username}</span>
+    );
+    // contentEditableDiv.append(doGeneateMentionableUser);
+    pasteHtmlAtCaret(
+      `<span data-key="1">${getMentionableUserDetails.username}</span> `
+    );
+  };
+
+  const pasteHtmlAtCaret = (html, selectPastedContent = false) => {
+    var sel, range;
+    if (window.getSelection) {
+      // IE9 and non-IE
+      sel = window.getSelection();
+      if (sel.getRangeAt && sel.rangeCount) {
+        range = sel.getRangeAt(0);
+        range.deleteContents();
+
+        // Range.createContextualFragment() would be useful here but is
+        // only relatively recently standardized and is not supported in
+        // some browsers (IE9, for one)
+        var el = document.createElement("div");
+        el.innerHTML = html;
+        var frag = document.createDocumentFragment(),
+          node,
+          lastNode;
+        while ((node = el.firstChild)) {
+          lastNode = frag.appendChild(node);
+        }
+        var firstNode = frag.firstChild;
+        range.insertNode(frag);
+
+        // Preserve the selection
+        if (lastNode) {
+          range = range.cloneRange();
+          range.setStartAfter(lastNode);
+          if (selectPastedContent) {
+            range.setStartBefore(firstNode);
+          } else {
+            range.collapse(true);
+          }
+          sel.removeAllRanges();
+          sel.addRange(range);
+        }
+      }
+    } else if ((sel = document.selection) && sel.type != "Control") {
+      // IE < 9
+      var originalRange = sel.createRange();
+      originalRange.collapse(true);
+      sel.createRange().pasteHTML(html);
+      if (selectPastedContent) {
+        range = sel.createRange();
+        range.setEndPoint("StartToStart", originalRange);
+        range.select();
+      }
+    }
   };
 
   return (
@@ -184,6 +262,7 @@ export default function CustomContentEditable() {
       <button className="button" onClick={handleGetText}>
         Get Text
       </button>
+      <div style={{ margin: "50px auto" }}>{plainText}</div>
     </div>
   );
 }
